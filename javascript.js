b8272c79b672ba40cffee7ea8c8bd55122df8757
Jordan <42592124+JordanBrittain@users.noreply.github.com>
2023-11-22 12:35:55 -0600

    Created functions for determining choices and winner

--- javascript.js
+++ javascript.js
@@ -1,34 +1,45 @@
-//function that returns either rock, paper, or scissors randomly
-//function that prompts user to input rock, paper or scissors
-//function that compares results of the above two functions, and returns winner
-
-
 //create computer choice function
 function getComputerChoice() {
-    const options = ['Rock', 'Paper', 'Scissors'];
-    const choice = Math.floor(Math.random() * options.length);
-    return options[choice];
+  const options = ["rock", "paper", "scissors"];
+  const choice = Math.floor(Math.random() * options.length);
+  return options[choice];
 }
 
 //capture user choice function
 function getUserChoice() {
-    return prompt('Rock, Paper, or Scissors?');
+  const userChoice = prompt("Rock, Paper, or Scissors?");
+  return userChoice.toLowerCase();
 }
 
-let computerSelection = getComputerChoice();
-let userSelection = getUserChoice();
-console.log(computerSelection);
-console.log(userSelection);
-
-
-//logic for determining winner
-if (userSelection === computerSelection) {
-    console.log('Its a tie');
-} else if ((userSelection == 'Rock' && computerSelection == 'Scissors')
-|| (userSelection == 'Scissors' && computerSelection == 'Paper')
-|| (userSelection == 'Paper' && computerSelection == 'Rock')) {
-    console.log('You win!');
-} else {
-    console.log('You lose :(');
+//function for determining winner
+function getWinner() {
+  if (userSelection === computerSelection) {
+    return "Its a tie"
+  } else if (
+    (userSelection === "rock" && computerSelection === "scissors") ||
+    (userSelection === "scissors" && computerSelection === "paper") ||
+    (userSelection === "paper" && computerSelection === "rock")
+  ) {
+    return "You win!"
+  } else {
+    return "You lose :("
+  }
 }
+
+
+//game function
+//let user score
+//let computer score
+//get user choice, get computer choice, get winner.
+//get winner function increments score variables
+//maybe console log scores
+//check to see if user score + computer score = 5
+//if yes check which score is higher. Declare winner
+//if no then repeat function
+
+
+    let computerSelection = getComputerChoice();
+    let userSelection = getUserChoice();
+    console.log(getWinner());
+
 
