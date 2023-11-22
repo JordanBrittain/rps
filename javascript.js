2f74de49a4d184fad30f337f0717e5dd1ea41aa2
Jordan <42592124+JordanBrittain@users.noreply.github.com>
2023-11-22 11:54:41 -0600

    added logic for determining winner

--- javascript.js
+++ javascript.js
@@ -2,10 +2,33 @@
 //function that prompts user to input rock, paper or scissors
 //function that compares results of the above two functions, and returns winner
 
+
+//create computer choice function
 function getComputerChoice() {
     const options = ['Rock', 'Paper', 'Scissors'];
     const choice = Math.floor(Math.random() * options.length);
     return options[choice];
 }
 
-console.log(getComputerChoice());
+//capture user choice function
+function getUserChoice() {
+    return prompt('Rock, Paper, or Scissors?');
+}
+
+let computerSelection = getComputerChoice();
+let userSelection = getUserChoice();
+console.log(computerSelection);
+console.log(userSelection);
+
+
+//logic for determining winner
+if (userSelection === computerSelection) {
+    console.log('Its a tie');
+} else if ((userSelection == 'Rock' && computerSelection == 'Scissors')
+|| (userSelection == 'Scissors' && computerSelection == 'Paper')
+|| (userSelection == 'Paper' && computerSelection == 'Rock')) {
+    console.log('You win!');
+} else {
+    console.log('You lose :(');
+}
+
